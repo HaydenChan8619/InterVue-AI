1814cbed5cee8c3a31a0bfa052b3b94af2b6a160
Hayden Chan <haydenphchan@gmail.com>
2025-08-16 11:28:32 -0700

    Logo Added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 const Footer = () => {
   return (
     <footer className="bg-slate-900 text-white py-12 px-4">
@@ -6,10 +7,9 @@ const Footer = () => {
         <div className="grid grid-cols-1 md:grid-cols-1 gap-8">
           <div>
             <div className="flex items-center space-x-2 mb-6">
-              <div className="bg-indigo-600 text-white p-2 rounded-lg">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z" />
-                </svg>
+              <div className="text-white p-2 rounded-lg">
+                <Image src='/logo.png' width={40} height={40} alt='InterVue AI Logo'
+                className='pt-2'/>
               </div>
               <span className="text-xl font-bold">InterVue AI</span>
             </div>
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import Image from 'next/image';
 
 const NavBar = () => {
     const [isScrolled, setIsScrolled] = useState(false);
@@ -28,8 +29,8 @@ const NavBar = () => {
         className="flex items-center space-x-2 cursor-pointer transition-transform duration-200 hover:scale-105"
         onClick={handleGetHome}
         >
-        <div className="bg-indigo-600 text-white p-2 rounded-lg transition-colors duration-200 hover:bg-indigo-700">
-            <svg
+        <div className="text-white p-2 rounded-lg transition-colors duration-200">
+            {/*<svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-6 w-6"
             fill="none"
@@ -42,7 +43,9 @@ const NavBar = () => {
                 strokeWidth={2}
                 d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"
             />
-            </svg>
+            </svg>*/}
+            <Image src='/logo.png' width={40} height={40} alt='InterVue AI Logo'
+            className='pt-2'/>
         </div>
         <span className="text-xl font-bold text-indigo-900">InterVue AI</span>
         </div>
